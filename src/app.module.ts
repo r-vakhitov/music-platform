dc31bcfe5c0f6adde504f8a082cb55e039341f51
Roman <outpacroman@gmail.com>
2022-01-06 15:33:23 +0300

    final version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import {Module} from "@nestjs/common";
 import {MongooseModule} from "@nestjs/mongoose";
 import {TrackModule} from "./track/track.module";
+import {FileModule} from "./files/file.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path';
 
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.i6wgz.mongodb.net/music-platform?retryWrites=true&w=majority'),
     TrackModule,
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.i6wgz.mongodb.net/music-platform?retryWrites=true&w=majority')
+    FileModule
   ]
 })
 export class AppModule {}
